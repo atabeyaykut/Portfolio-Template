eea020526c5d60d2e5ae2ac665d09f37d1060b48
kantlori <atabeywade@gmail.com>
2025-02-06 23:10:54 +0300

    Add clsx dependency; update Projects component styling and restructure data.json for projects and footer

--- src/components/HomePage/Projects/Projects.jsx
+++ src/components/HomePage/Projects/Projects.jsx
@@ -6,8 +6,8 @@ function Projects() {
     const { user } = useUser()
 
     return (
-        <div className='container mx-auto px-4 py-12'>
-            <h2 className='text-3xl font-semibold text-center mb-8'>{user.projects.title}</h2>
+        <div className='container mx-auto px-10 py-12'>
+            <h2 className='text-3xl font-semibold text-center pb-4 mb-8'>{user.projects.title}</h2>
 
             <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
                 {Object.values(user.projects.content).map((project, index) => (
